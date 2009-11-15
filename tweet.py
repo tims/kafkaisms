c2153818f540277ca6074f42119cc0d7dd0e1137
Tim <tims@timsmini.(none)>
2009-11-15 22:28:17 +0000

    add time message

--- tweet.py
+++ tweet.py
@@ -14,6 +14,7 @@ if status == None:
 
 api = twitter.Api(username=username, password=password)
 
+print "Kafkaisms tweeter.", datetime.datetime.now()
 bla = api.GetUserTimeline(username, count=1)
 if len(bla) > 0:
     livetweet = [long(bla[0].id), datetime.datetime.strptime(bla[0].created_at, "%a %b %d %H:%M:%S +0000 %Y"), bla[0].text]
