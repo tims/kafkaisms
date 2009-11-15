0180df2821f80dc346f86bbca639a734b956e10e
Tim <tims@timsmini.(none)>
2009-11-15 20:58:24 +0000

    readme and blank status

---
+++ readme.txt
@@ -0,0 +1,5 @@
+Tweet the metamorphosis, by franz kafka.
+tweets.py should be run every 30 minutes in a cron job.
+
+To regen the tweets file, do the following:
+cat metamorphosis.txt | py split.py | py group.py | py time.py > metamorphosis_tweets.txt
--- tweet.py
+++ tweet.py
@@ -1,17 +1,38 @@
 import yaml
 import twitter
 import datetime
-#api = twitter.Api()
-#print api.GetUserTimeline('roserpens')
+import sys
+
+config = yaml.load(open('config.yaml'))
+username = config['username']
+password = config['password']
 
 status = yaml.load(open('status.yaml'))
 if status == None:
     status = {}
+
+api = twitter.Api(username=username, password=password)
+
+bla = api.GetUserTimeline('kafkaisms', count=1)
+if len(bla) > 0:
+    livetweet = [long(bla[0].id), datetime.datetime.strptime(bla[0].created_at, "%a %b %d %H:%M:%S +0000 %Y"), bla[0].text]
+else:
+    livetweet = [None, None, None]
+liveid, livedate, livetext = livetweet
+
 lasttweet = status.get('lasttweet', {})
+lastid = lasttweet.get('id',0)
 lastdate = lasttweet.get('date', datetime.datetime.fromtimestamp(0))
 lasttext = lasttweet.get('text')
 
-print "lasttweet:",lastdate,lasttext
+print "livetweet:",liveid, livedate, livetext
+print "lasttweet:",lastid, lastdate, lasttext
+
+if lastid > 0:
+    if lastid != liveid:
+        raise Exception("live id and last id don't match!")
+    if lasttext != livetext:
+        raise Exception("live text and last text don't match!")
 
 tweetsfile = open('metamorphosis_tweets.txt')
 for line in tweetsfile:
@@ -23,9 +44,18 @@ for line in tweetsfile:
 
 print "nexttweet:",dt,text
 
-# do post to twitter
+if dt < datetime.datetime.now():
+    print "past publish time, publishing"
+else:
+    print "not yet publish time."
+    sys.exit()
 
-status['lasttweet'] = {'date':dt, 'text':text}
+# do post to twitter
+poststatus = api.PostUpdate(text)
+print poststatus
+id = long(poststatus.id)
+
+status['lasttweet'] = {'id':id, 'date':dt, 'text':text}
 yaml.dump(status, stream=open('status.yaml','w'))
 
 
